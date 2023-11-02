9c55f4dbfa9b8e8aa270d9ce8ff03c5feba53086
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-02 09:14:55 -0300

    - Criada área de importação de bibliotecas - Criada área de variáveis iniciais - Criado menu inicial do jogo

--- main.py
+++ main.py
@@ -16,6 +16,41 @@
 #  v0.0.1 02-11-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho
+#   - Criada área de importação de bibliotecas
+#   - Criada área de variáveis iniciais
+#   - Criado menu inicial do jogo
 #
 # Licença: MIT.
 #
+
+# Impotando bibliotecas
+
+from os import system
+from time import sleep
+
+# Impotando bibliotecas ###
+# Variáveis iniciais.
+
+PlayerOne = str('PlayerOne')
+
+MenuCH = int(0)
+
+MenuOpcao = int(0)
+
+# Variáveis íniciais. ###
+# Menu ínicial
+while MenuCH != 1:
+    # Limpa a tela
+    system('export TERM=xterm ; clear') or None
+    MenuOpcao = int(input('1 - Iniciar\n2 - Carregar(Não implementado)\n3 - Opções(Não implementado)\n>>> '))
+    if MenuOpcao == 1:
+        MenuCH = int(1)
+    else:
+        # Limpa a tela
+        system('export TERM=xterm ; clear') or None
+        print('Opção inválida!')
+        sleep(2)
+# Menu ínicial ###
+#
+
+
